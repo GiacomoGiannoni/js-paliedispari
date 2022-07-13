4e91dff3b12b48f9713e71f9b280ce168f6d64e1
Giacomo Giannoni <giacomogiannoni7@gmail.com>
2022-07-13 10:36:30 +0200

    correzioni

--- palindromi/js/main.js
+++ palindromi/js/main.js
@@ -1,11 +1,11 @@
-const parola = prompt("Inserisci una parola");
+/*const parola = prompt("Inserisci una parola");
 let palindroma = isPalindroma(parola);
 function isPalindroma(sillabe){ 
     let temp=[];
     for(let i = sillabe.length; i >= 0; i--) {
       temp.push(sillabe[i]);
     }
-    for(let i=0;i<parola.length;i++) {
+    for(let i = 0; i < parola.length ; i++) {
        if(parola[i] != temp[i+1]) {
         alert('NON Palindroma')
        } else {
@@ -13,4 +13,24 @@ function isPalindroma(sillabe){
        }
        return;
     }
+}
+*/
+
+
+//correzione
+
+const parola = prompt('inserisci la parola che vuoi testare');
+const parolaGirata = giraParola(parola);
+if (parola == parolaGirata) {
+  alert('Palindroma');
+} else {
+  alert('NON Palindroma');
+}
+
+function giraParola(pippo) {
+  let parolaGirata = '';
+  for (let 1 = pippo.lenght - 1; i>= 0; i--) {
+    parolaGirata += pippo[1];
+  }
+  return parolaGirata;
 }
--- pari_e_dispari/js/main.js
+++ pari_e_dispari/js/main.js
@@ -1,15 +1,12 @@
+/*
 const buttons = document.querySelectorAll(".box");
-
 const userTextBox= document.getElementById("user-text");
 const endText = document.getElementById("end-text");
-
 let userPosition;
 let computerPosition;
-
 let userNumber;
 let computerNumber;
 let sum = 0;
-
 for(let element of buttons) {
     element.addEventListener("click",function(){    
         if(element.id == "pari") {
@@ -22,7 +19,6 @@ for(let element of buttons) {
         enterNumber();
     })
 }
-
 function enterNumber() {
     userNumber=parseInt(prompt("Inserisci un numero da 1 a 5"));
     while(userNumber<1 || userNumber>5){
@@ -31,13 +27,11 @@ function enterNumber() {
     userTextBox.textContent = userNumber;
     computerNumberGenerator();
 }
-
 function computerNumberGenerator() {
     computerNumber = Math.floor(Math.random() * 5)+1;
     userTextBox.innerHTML += " Vs "+computerNumber;
     whoIsTheWinner();
 }
-
 function whoIsTheWinner() {
     sum = userNumber + computerNumber;
     endText.textContent = `il totale è ${sum}`;
@@ -49,5 +43,31 @@ function whoIsTheWinner() {
     }else{
         endText.innerHTML += ` VINCE IL COMPUTER`;
     }
+}*/
+
+//correzione
+const sceltaPariDispari = prompt('scegli p o d');
+const numeroScelta = parseInt(prompt('inserisci un numbero compreso tra 1 e 5'));
+
+const numeroComputer = getRandomNumber(1,5);
+const sommaNumeri = numeroComputer + numeroScelta;
+const risultatoPariODispari = checkPariDispari(sommaNumeri);
+console.log(numeroComputer);
+
+if (sceltaPariDispari == risultatoPariODispari) {
+    alert('Hai vinto!');
+} else {
+    alert('Ha vinto il computer!');
 }
 
+function checkPariDispari (numero) {
+    if (numero % 2 == 0) {
+        return 'pari';
+    } else {
+        return 'dispari';
+    }
+}
+
+function getRandomNumber(min, max) {
+    return (Math.floor(Math.random() * (max - min + 1)) + min);
+}
